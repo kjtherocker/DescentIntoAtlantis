08feff48aecb8014b4d27029d9b02d5c4d84bea1
kjtherocker <kylejohnsonthegamedev@gmail.com>
2025-04-03 20:50:57 -0400

    started work on passive interrupt

---
+++ DIA_5.4/Source/DesentIntoAtlantis/Public/PassiveInterrupt.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CombatInterruptData.h"
+#include "PassiveInterrupt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESENTINTOATLANTIS_API UPassiveInterrupt : public UCombatInterrupt
+{
+	GENERATED_BODY()
+	
+};
