1d600e39e5e10000ae2cc6cd0f1137b83f1ef242
kjtherocker <kylejohnsonthegamedev@gmail.com>
2025-04-10 12:44:22 -0400

    added once per combat abilitys

--- DIA_5.4/Source/DesentIntoAtlantis/Private/GenTriggerPassive_PerCombat_Resurrection.cpp
+++ DIA_5.4/Source/DesentIntoAtlantis/Private/GenTriggerPassive_PerCombat_Resurrection.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GenTriggerPassive_Resurrection.h"
+#include "GenTriggerPassive_PerCombat_Resurrection.h"
 
 
-FCombatLog_PassiveSkilData UGenTriggerPassive_Resurrection::ActivateGenericPassive_Implementation(UCombatEntity* WhoTriggeredEvent)
+FCombatLog_PassiveSkilData UGenTriggerPassive_PerCombat_Resurrection::ActivateGenericPassive_Implementation(UCombatEntity* WhoTriggeredEvent)
 {
 
 	attachedCombatEntity->Resurrection();
